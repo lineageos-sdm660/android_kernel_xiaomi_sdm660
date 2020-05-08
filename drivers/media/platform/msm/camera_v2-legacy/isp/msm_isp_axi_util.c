1e6386cc9b9ca7e556954c20b03b84c6c8b27b24
dianlujitao <dianlujitao@lineageos.org>
2020-05-08 17:51:21 +0800

    media: msm: camera_v2-legacy: conditionality eliminate drop_reconfig flag
    The flag is set to notify userspace driver, and expected to be clear
    there. However, our userspace driver isn't able to handle this, as a
    result the camera stopped working once it's set because an empty frame
    is always returned.
    
    This conditionality reverts bc82952bb8012641c5e74a228ab697ff6eec38e4
    and 39e7df335515c3f11859901d650d47ee983fcf6a
    
    Change-Id: I598d8ffc5f2d8eb1fe387a5210e0ef97d2e3badd

--- drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp_axi_util.c
@@ -3647,6 +3647,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		) {
 		pr_debug("%s:%d invalid time to request frame %d\n",
 			__func__, __LINE__, frame_id);
+#ifdef CONFIG_MACH_XIAOMI_JASON
+		goto error;
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active) &&
 			((frame_id ==
@@ -3659,6 +3662,8 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, vfe_dev->pdev->id, frame_id,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].active);
+		return 0;
+#endif
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active && (frame_id !=
 		vfe_dev->axi_data.src_info[frame_src].frame_id + vfe_dev->
 		axi_data.src_info[frame_src].sof_counter_step)) ||
@@ -3683,7 +3688,7 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, __LINE__, vfe_dev->pdev->id, frame_id,
 			stream_info->activated_framedrop_period,
 			stream_info->stream_id);
-
+#ifdef CONFIG_MACH_XIAOMI_JASON
 		rc = msm_isp_return_empty_buffer(vfe_dev, stream_info,
 			user_stream_id, frame_id, buf_index, frame_src);
 		if (rc < 0)
@@ -3692,6 +3697,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		stream_info->current_framedrop_period =
 			MSM_VFE_STREAM_STOP_PERIOD;
 		msm_isp_cfg_framedrop_reg(stream_info);
+#else
+		vfe_dev->isp_page->drop_reconfig = 1;
+#endif
 		return 0;
 	}
 
